8391ef8fe892d827048691c08c374587cac392d1
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-07-14 18:08:20 +0200

    Loaded images in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,12 @@ module.exports = {
         },
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/i,
-          type: 'asset/resource'
-        }
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
       ]
     },
     plugins: [
