72b4ae06d22e9888e33583580f03a9e47a345a43
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-07-16 15:41:20 +0200

    Added call to action button functionality

--- src/home.js
+++ src/home.js
@@ -1,7 +1,8 @@
 import './style.css'
 import icon from './img/icon.svg'
+import openMenuTab from './menu'
 
-export default function openHomeTab(){
+export default function openHomeTab() {
     const content = document.getElementById('content')
     content.classList.remove('menu-content')
     content.classList.add('content')
@@ -14,36 +15,44 @@ export default function openHomeTab(){
     const containerLeft = document.createElement('div')
     containerLeft.classList.add('container-left')
 
-        const headlineTop = document.createElement('div')
-        headlineTop.classList.add('headline-top')
-        headlineTop.innerHTML = 'Delicious'
+    const headlineTop = document.createElement('div')
+    headlineTop.classList.add('headline-top')
+    headlineTop.innerHTML = 'Delicious'
 
-        containerLeft.appendChild(headlineTop)
+    containerLeft.appendChild(headlineTop)
 
-        const headline = document.createElement('div')
-        headline.classList.add('headline')
-        headline.innerHTML = 'BURGER'
+    const headline = document.createElement('div')
+    headline.classList.add('headline')
+    headline.innerHTML = 'BURGER'
 
-        containerLeft.appendChild(headline) 
+    containerLeft.appendChild(headline)
 
-        const text = document.createElement('div')
-        text.classList.add('text')
-        text.innerHTML = 'Indulge in mouthwatering, handcrafted burgers that will satisfy your cravings and leave you wanting more.'
+    const text = document.createElement('div')
+    text.classList.add('text')
+    text.innerHTML = 'Indulge in mouthwatering, handcrafted burgers that will satisfy your cravings and leave you wanting more.'
 
-        containerLeft.appendChild(text) 
+    containerLeft.appendChild(text)
 
-        const buttonOrder = document.createElement('button')
-        buttonOrder.classList.add('cssbuttons-io-button')
-        buttonOrder.id = 'orderBtn'
-        buttonOrder.innerHTML = 'ORDER NOW'
-            const btnIcon = document.createElement('div')
-            btnIcon.classList.add('icon')
-                const arrowIcon = new Image()
-                arrowIcon.src = icon
+    const buttonOrder = document.createElement('button')
+    buttonOrder.classList.add('cssbuttons-io-button')
+    buttonOrder.id = 'orderBtn'
+    buttonOrder.innerHTML = 'ORDER NOW'
+    const btnIcon = document.createElement('div')
+    btnIcon.classList.add('icon')
+    const arrowIcon = new Image()
+    arrowIcon.src = icon
 
-                btnIcon.appendChild(arrowIcon)
-        buttonOrder.appendChild(btnIcon)
-        containerLeft.appendChild(buttonOrder) 
+    buttonOrder.addEventListener('click', () => {
+        homeTab.classList.remove('tab-active')
+        contactTab.classList.remove('tab-active')
+        menuTab.classList.add('tab-active')
+
+        openMenuTab();
+    })
+
+    btnIcon.appendChild(arrowIcon)
+    buttonOrder.appendChild(btnIcon)
+    containerLeft.appendChild(buttonOrder)
 
     mainSection.appendChild(containerLeft)
 
--- src/index.js
+++ src/index.js
@@ -36,6 +36,5 @@ window.onload = function () {
             openMenuTab();
         }
     })
-
 };
 
